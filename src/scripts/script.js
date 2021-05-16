fbbbc14bbee139855c3bb5cdab70317f6b41bc2e
samx23 <skalexsong@gmail.com>
2021-05-16 21:14:05 +0700

    Update : full fill basic rules and half working state

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -17,7 +17,6 @@ class Human extends Player {
     this.batu[0].id = "batu-player";
     this.kertas[0].id = "kertas-player";
     this.gunting[0].id = "gunting-player";
-    console.log("Human loaded");
   }
 }
 
@@ -31,7 +30,6 @@ class Bot extends Player {
     this.batu[1].id = "batu-com";
     this.kertas[1].id = "kertas-com";
     this.gunting[1].id = "gunting-com";
-    console.log("Com loaded");
   }
 }
 
@@ -41,7 +39,7 @@ class Rules {
     this.resultContainer = document.getElementById("vs_result");
   }
 
-  defaultState = () => {
+  defaultResult = () => {
     this.resultText.innerHTML = "VS";
     this.resultContainer.appendChild(this.resultText);
     this.resultContainer.classList.remove("versus_result");
@@ -68,12 +66,31 @@ class Rules {
     this.resultContainer.classList.add("versus_result");
     this.resultContainer.classList.add("draw");
   };
+
+  decision = (userChoice, botChoice) => {
+    if (userChoice === botChoice) {
+      return this.drawResult();
+    } else if (
+      (userChoice === "batu" && botChoice === "gunting") ||
+      (userChoice === "kertas" && botChoice === "batu") ||
+      (userChoice === "gunting" && botChoice === "kertas")
+    ) {
+      return this.winResult();
+    } else if (
+      (userChoice === "batu" && botChoice === "kertas") ||
+      (userChoice === "kertas" && botChoice === "gunting") ||
+      (userChoice === "gunting" && botChoice === "batu")
+    ) {
+      return this.loseResult();
+    }
+  };
 }
 
 class Game extends Rules {
   constructor() {
     super();
     this.result = [];
+    this.playerHasPick = false;
     this.resetResult = document.getElementById("reset");
     this._initiation();
   }
@@ -81,103 +98,98 @@ class Game extends Rules {
   _initiation() {
     this.user = new Human();
     this.com = new Bot();
-    this.listener();
+    this.defaultResult();
+    this.reset();
   }
 
   reset() {
     this.resetResult.addEventListener("click", () => {
-      this.defaultState();
+      this.defaultResult();
       this.result = [];
+      this.playerHasPick = false;
+      this.user.batu[0].classList.remove("active_choice");
+      this.user.kertas[0].classList.remove("active_choice");
+      this.user.gunting[0].classList.remove("active_choice");
       console.log("reset");
     });
   }
 
-  listener() {
-    this.reset();
+  getUserPick = (choice) => {
+    this.user.choice = choice;
+    this.playerHasPick = true;
+    console.log("Player pick: ", this.user.choice);
+    return this.user.choice;
+  };
 
-    const getUserPick = (choice) => {
-      this.user.choice = choice;
-      console.log(this.user.choice);
-      return this.user.choice;
-    };
+  getComPick = (choice) => {
+    this.com.choice = choice;
+    this.comHasPick = true;
+    console.log("Com pick: ", this.com.choice);
+    return this.com.choice;
+  };
 
-    const getComPick = (choice) => {
-      this.com.choice = choice;
-      console.log(this.com.choice);
-      return this.com.choice;
-    };
-
-    // User listener section
+  listener = () => {
     this.user.batu[0].addEventListener("click", () => {
-      // this.user.batu[0].classList.add("active_choice");
-      this.result[0] = getUserPick("batu");
-      this.decision();
+      this.result[0] = this.getUserPick("batu");
+      this.decideResult();
     });
 
     this.user.kertas[0].addEventListener("click", () => {
-      this.result[0] = getUserPick("kertas");
-      this.decision();
+      this.result[0] = this.getUserPick("kertas");
+      this.decideResult();
     });
 
     this.user.gunting[0].addEventListener("click", () => {
-      this.result[0] = getUserPick("gunting");
-      this.decision();
+      this.result[0] = this.getUserPick("gunting");
+      this.decideResult();
     });
 
-    // Com listener section
-    this.com.batu[1].addEventListener("click", () => {
-      this.result[1] = getComPick("batu");
-      this.decision();
-    });
-    this.com.kertas[1].addEventListener("click", () => {
-      this.result[1] = getComPick("kertas");
-      this.decision();
-    });
-    this.com.gunting[1].addEventListener("click", () => {
-      this.result[1] = getComPick("gunting");
-      this.decision();
-    });
-  }
+    if (this.playerHasPick) {
+      this.com.batu[1].addEventListener("click", () => {
+        this.result[1] = this.getComPick("batu");
+        this.decideResult();
+      });
+      this.com.kertas[1].addEventListener("click", () => {
+        this.result[1] = this.getComPick("kertas");
+        this.decideResult();
+      });
+      this.com.gunting[1].addEventListener("click", () => {
+        this.result[1] = this.getComPick("gunting");
+        this.decideResult();
+      });
+    }
+  };
 
-  play() {
-    console.log("Lets play!");
-    this.decision();
-  }
-
-  decision() {
+  decideResult() {
     console.log("Current result: ", this.result);
     switch (this.user.choice) {
       case "batu":
         this.winResult();
+        this.user.batu[0].classList.add("active_choice");
+        this.user.kertas[0].classList.remove("active_choice");
+        this.user.gunting[0].classList.remove("active_choice");
         break;
       case "kertas":
         this.loseResult();
+        this.user.batu[0].classList.remove("active_choice");
+        this.user.kertas[0].classList.add("active_choice");
+        this.user.gunting[0].classList.remove("active_choice");
         break;
       case "gunting":
         this.drawResult();
+        this.user.batu[0].classList.remove("active_choice");
+        this.user.kertas[0].classList.remove("active_choice");
+        this.user.gunting[0].classList.add("active_choice");
         break;
       default:
-        this.defaultState();
+        this.defaultResult();
         break;
     }
+  }
 
-    // if (this.result.length <= 2) {
-    //   this.listener();
-    // switch (this.user.choice) {
-    //   case "batu":
-    //     this.winResult();
-    //     break;
-    //   case "kertas":
-    //     this.loseResult();
-    //     break;
-    //   case "gunting":
-    //     this.drawResult();
-    //     break;
-    //   default:
-    //     this.defaultState();
-    //     break;
-    // }
-    // }
+  play() {
+    console.log("Lets play!");
+    this.listener();
   }
 }
 
